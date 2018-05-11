bb49f4de865f820f9420a6b56e81ba9682d27235
knolljs <33844799+knolljs@users.noreply.github.com>
2018-05-11 18:19:06 -0400

    Add files via upload

---
+++ Audio_Visualizer/Audio_Visualizer.ino
@@ -0,0 +1,38 @@
+#include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
+#define PIN 6
+SoftwareSerial BTSerial(2,3);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30,PIN,NEO_GRB + NEO_KHZ800);
+int toggle = 0;
+int i = 0;
+void setup() {
+  BTSerial.begin(38400);    // 38400 is the default baud rate for the serial Bluetooth module
+  Serial.begin(9600);
+  strip.begin();
+  strip.show();
+}
+void loop() {
+  // listen for the data
+  if ( BTSerial.available() > 0 ) {
+    toggle = BTSerial.read();
+    Serial.println(toggle);
+    if(toggle<=60 && toggle!=0){
+        strip.setPixelColor(i,strip.Color(0,255,0));
+    }
+    if(toggle>60 && toggle<=120){
+        strip.setPixelColor(i,strip.Color(255,0,0));
+    }
+    if(toggle>120){
+        strip.setPixelColor(i,strip.Color(0,0,255));
+    }
+    if(i>=30){
+      i = 0;
+      strip.show();
+    }
+    else{
+      i++;
+    }
+  }
+}
+
+
